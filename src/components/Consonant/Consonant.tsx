7f948d2777a0e5c4204a1c533026d4581362d741
marcus pedersen <marcus@mpedersen.me>
2025-02-13 00:26:24 +0100

    Create interface for prop

--- src/components/Consonant/Consonant.tsx
+++ src/components/Consonant/Consonant.tsx
@@ -1,6 +1,11 @@
 import { ConsonantType } from "../../types";
+import styles from "./Consonant.module.css";
 
-const Consonant = ({ consonant }: ConsonantType) => {
+interface ConsonantProps {
+  consonant: ConsonantType;
+}
+
+const Consonant = ({ consonant }: ConsonantProps) => {
   return (
     <li key={consonant.id} className={styles.listItem}>
       <h2 className={styles.character}>{consonant.character}</h2>
